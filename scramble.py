b7e733904295781e421845a5430bc05b485d065f
zaw win aung <zawaung7791@users.noreply.github.com>
2025-08-14 22:53:29 +0100

    Delete scramble.py

--- scramble.py
+++
@@ -1,6 +0,0 @@
-import polars as pl
-
-load_df = pl.read_parquet("..\\..\\pricing-exploratory-data-analysis\\data\\2022_2024_copa_product_customer.parquet")
-
-scrambled_df = load_df.sample(n=load_df.height, with_replacement=False)
-scrambled_df.write_parquet("..\\..\\pricing-exploratory-data-analysis\\data\\scrambled_2022_2024_copa_product_customer.parquet")
